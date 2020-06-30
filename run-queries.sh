6a9d9575996ea94322ebdf74746f4aaaa5ac365d
Martin Kellogg <kelloggm@cs.washington.edu>
2020-06-30 10:34:00 -0700

    shellcheck on other scripts, and shellcheck makefile

--- run-queries.sh
+++ run-queries.sh
@@ -17,7 +17,7 @@
 
 query_file=$1
 
-if [ -z ${query_file} ]; then
+if [ -z "${query_file}" ]; then
     echo "you must have provide a query file as the first argument"
     exit 2
 fi
@@ -28,7 +28,7 @@ else
     page_count=$2
 fi
 
-query=`cat ${query_file} | tr ' ' '+'`
+query=$(tr ' ' '+' < "${query_file}")
 
 ## for storing the results before sorting and uniqing them
 rm -f /tmp/github-query-results.txt
@@ -40,11 +40,8 @@ hashfile=$(mktemp /tmp/github-hash-results.txt)
 #trap "rm -f ${hashfile}" 0 2 3 15
 
 # find the repos
-for i in `seq ${page_count}`; do
+for i in $(seq "${page_count}"); do
     full_query='https://api.github.com/search/code?q='${query}'&page='${i}
-#    echo ${full_query}
-    #    exit 1
-
     # this removes projects that are
     # 1. owned by me
     # 2. are hard-forks of android-libcore, because they're very big and
@@ -53,33 +50,32 @@ for i in `seq ${page_count}`; do
     # 4. are owned by the user AndroidSDKSources, because those
     #    are all copies of (surprise!) the android SDK, which we
     #    don't care about for the same reasons.
-    curl -sH "Authorization: token `cat git-personal-access-token`" \
-     	     "Accept: application/vnd.github.v3+json" \
-     	     ${full_query} \
-    	| grep "        \"html_url" \
-    	| grep -v "          " \
-    	| sort | uniq \
-    	| cut -d \" -f 4 \
-    	| grep -v "kelloggm" \
+    curl -sH "Authorization: token $(cat git-personal-access-token)" \
+              "Accept: application/vnd.github.v3+json" \
+             "${full_query}" \
+        | grep "        \"html_url" \
+        | grep -v "          " \
+        | sort | uniq \
+        | cut -d \" -f 4 \
+        | grep -v "kelloggm" \
         | grep -v "libcore" \
-	| grep -v "apache-harmony" \
-	| grep -v "AndroidSDKSources" >> ${tempfile}
+    | grep -v "apache-harmony" \
+    | grep -v "AndroidSDKSources" >> "${tempfile}"
 done
 
-sort -u -o ${tempfile} ${tempfile}
+sort -u -o "${tempfile}" "${tempfile}"
 
 while IFS= read -r line
 do
-    repo=`echo ${line} | cut -d / -f 5`
-    owner=`echo ${line} | cut -d / -f 4`
+    repo=$(echo "${line}" | cut -d / -f 5)
+    owner=$(echo "${line}" | cut -d / -f 4)
     hash_query='https://api.github.com/repos/'${owner}'/'${repo}'/commits?per_page=1'
-#    echo ${hash_query}
-    curl -sH "Authorization: token `cat git-personal-access-token`" \
-    	     "Accept: application/vnd.github.v3+json" \
-    	     ${hash_query} \
-    	| grep '^    "sha":' \
-    	| cut -d \" -f 4 >> ${hashfile}
+    curl -sH "Authorization: token $(cat git-personal-access-token)" \
+             "Accept: application/vnd.github.v3+json" \
+             "${hash_query}" \
+        | grep '^    "sha":' \
+        | cut -d \" -f 4 >> "${hashfile}"
     
-done < ${tempfile}
+done < "${tempfile}"
 
-paste ${tempfile} ${hashfile}
+paste "${tempfile}" "${hashfile}"
--- summary.sh
+++ summary.sh
@@ -6,28 +6,19 @@
 
 target=$1
 
-total=`ls -l ${target}/*.log | wc -l`
+total=$(find "${target}" -name "*.log" | wc -l)
 
-timed_out=`grep -l "dljc timed out for" ${target}/*.log | wc -l`
-timed_out_percent=$(((${timed_out}*100)/${total}))
-timed_out_list=`grep -l "dljc timed out for" ${target}/*.log`
+timed_out=$(grep -cl "dljc timed out for" "${target}/*.log")
+timed_out_percent=$(((timed_out*100)/total))
+timed_out_list=$(grep -l "dljc timed out for" "${target}/*.log")
 
-no_build=`grep -l "no build file found for" ${target}/*.log | wc -l`
-no_build_percent=$(((${no_build}*100)/${total}))
+no_build=$(grep -cl "no build file found for" "${target}/*.log")
+no_build_percent=$(((no_build*100)/total))
 
-## These checks are no longer necessary, because DLJC now handles both cases
-## automatically and is therefore able to process such projects.
-
-# lombok=`grep -lZ 'cannot find symbol' ${target}/*.log | xargs -0 grep -l 'lombok' | wc -l`
-# lombok_percent=$(((${lombok}*100)/${total}))
-
-# no_java_under_8=`grep -l "error: Source option" ${target}/*.log | wc -l`
-# no_java_under_8_percent=$(((${no_java_under_8}*100)/${total}))
-
-no_cf_old=`grep -l "dljc could not run the Checker Framework" ${target}/*.log | wc -l`
-no_cf_new=`grep -l "dljc could not run the build successfully" ${target}/*.log | wc -l`
-no_cf=$((${no_cf_old}+${no_cf_new}))
-no_cf_percent=$(((${no_cf}*100)/${total}))
+no_cf_old=$(grep -cl "dljc could not run the Checker Framework" "${target}/*.log")
+no_cf_new=$(grep -cl "dljc could not run the build successfully" "${target}/*.log")
+no_cf=$((no_cf_old+no_cf_new))
+no_cf_percent=$(((no_cf*100)/total))
 
 echo "total repositories: ${total} (100%)"
 echo "no maven or gradle build file: ${no_build} (~${no_build_percent}%)"
@@ -36,25 +27,20 @@ echo "timed out: ${timed_out} (~${timed_out_percent}%)"
 echo ""
 echo "timeouts:"
 echo ""
-echo ${timed_out_list} | tr ' ' '\n'
+echo "${timed_out_list}" | tr ' ' '\n'
 echo ""
-# echo "build failed due to lombok: ${lombok} (~${lombok_percent}%)"
-# echo "Java 7 or earlier: ${no_java_under_8} (~${no_java_under_8_percent}%)"
 
-unaccounted_for=`grep -Zvl "no build file found for" ${target}/*.log \
+unaccounted_for=$(grep -Zvl "no build file found for" "${target}/*.log" \
     | xargs -0 grep -Zvl "dljc could not run the Checker Framework" \
     | xargs -0 grep -Zvl "dljc could not run the build successfully" \
     | xargs -0 grep -Zvl "dljc timed out for" \
-    | xargs -0 echo`
-
-unaccounted_for_count=`echo ${unaccounted_for} | tr ' ' '\n' | wc -l`
-unaccounted_for_count_percent=$(((${unaccounted_for_count}*100)/${total}))
+    | xargs -0 echo)
 
 echo "unaccounted for: "
 echo ""
-echo ${unaccounted_for} | tr ' ' '\n'
+echo "${unaccounted_for}" | tr ' ' '\n'
 echo ""
 
 echo "unaccounted for LoC:"
 
-cat ${target}/loc.txt
+cat "${target}/loc.txt"
