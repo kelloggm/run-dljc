a8a87f09a8f31f7d79ca99193880fbc15ae0d9d7
Michael Ernst <mernst@cs.washington.edu>
2020-07-02 17:02:07 -0700

    Formatting

--- no-literal-securerandom-exact-dljc-cmd.sh
+++ no-literal-securerandom-exact-dljc-cmd.sh
@@ -11,11 +11,13 @@
 ## configure-and-exec-dljc.sh directly rather than making a copy of
 ## this script.
 
-# Change these if you are running on a non-CSE machine.
+## Change these if you are running on a non-CSE machine.
+
 export JAVA11_HOME=/usr/lib/jvm/java-11-openjdk/
 export JAVA8_HOME=/usr/lib/jvm/java-1.8.0-openjdk
 
-# Everyone must change these.
+## Everyone must change these.
+
 export PARENTDIR=${HOME}/compliance-experiments/fse20
 export CHECKERFRAMEWORK=${PARENTDIR}/checker-framework
 checker=org.checkerframework.checker.noliteral.NoLiteralChecker
@@ -33,15 +35,17 @@ custom_quals='/homes/gws/kelloggm/compliance-experiments/fse20/no-literal-checke
 # by running ./gradlew printClasspath in the mychecker-checker subproject.
 custom_classpath='/homes/gws/kelloggm/compliance-experiments/fse20/no-literal-checker/no-literal-checker/build/classes/java/main:/homes/gws/kelloggm/compliance-experiments/fse20/no-literal-checker/no-literal-checker/build/resources/main:/homes/gws/kelloggm/compliance-experiments/fse20/checker-framework/checker/dist/checker.jar:/homes/gws/kelloggm/compliance-experiments/fse20/no-literal-checker/no-literal-qual/build/libs/no-literal-qual.jar:/homes/gws/kelloggm/.gradle/caches/modules-2/files-2.1/com.google.errorprone/javac/9+181-r4173-1/bdf4c0aa7d540ee1f7bf14d47447aea4bbf450c5/javac-9+181-r4173-1.jar:/homes/gws/kelloggm/.gradle/caches/modules-2/files-2.1/org.checkerframework/checker-qual/3.1.1/361404eff7f971a296020d47c928905b3b9c5b5f/checker-qual-3.1.1.jar'
 
-# Optionally change these.
+## Optionally change these.
+
 export ANDROID_HOME=${PARENTDIR}/android_home
 timeout=3600 # 60 minutes
 
-# There is no need to make changes below this point.
+## There is no need to make changes below this point.
+
 export JAVA_HOME=${JAVA11_HOME}
 repolistbase=$(basename "$repolist")
 
-# Code starts here.
+## Code starts here.
 
 rm -rf "${checkername}-${repolistbase}-results"
 
